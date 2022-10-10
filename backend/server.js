97610aac27073b91bb18d57cfc45ca63aa7ac28b
kanngji <50470748+kanngji@users.noreply.github.com>
2022-10-10 23:16:37 +0900

    ATLAS MONGODB 연결

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser"); // what is this?
-const cors = require("cors"); // what is this?
+const path = require("path");
+const cors = require("cors"); // what is this? react port와 node port data받을려고하는거같아용
 
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors()); // w i t?
 app.use(bodyParser.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(
+  uri,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("connected");
+    }
+  }
+);
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
